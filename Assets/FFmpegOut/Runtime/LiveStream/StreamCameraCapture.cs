ffd222f34f56638e587c363e00fa3087bebaf282
Eric Johnson <ejohnson@darkhive.com>
2024-07-08 11:31:07 -0500

    added MJPEG stream

--- Assets/FFmpegOut/Runtime/LiveStream/StreamCameraCapture.cs
+++ Assets/FFmpegOut/Runtime/LiveStream/StreamCameraCapture.cs
@@ -4,7 +4,7 @@ namespace FFmpegOut.LiveStream
 {
     public class StreamCameraCapture : CameraCapture
     {
-        [SerializeField] protected StreamPreset _streamPreset;
+        [SerializeField] public StreamPreset _streamPreset;
         [SerializeField] public string streamAddress;
 
         protected override FFmpegSession GetSession(int texWidth, int texHeight)
